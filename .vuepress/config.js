9fcbf3476b45592767f6577da95f680855fc80ca
yuahng <1773110951@qq.com>
2021-06-23 13:18:02 +0800

    yh

---
+++ .vuepress/config.js
@@ -0,0 +1,80 @@
+module.exports = {
+  "title": "XiaoQi Press",
+  "description": "XiaoQi 的学习笔记",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "首页",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "时间线",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "GitHub",
+        "link": "https://github.com/xiaoqiyamedia",
+        "icon": "reco-github"
+      }
+    ],
+    "sidebar": {
+      
+    },
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "分类"
+      },
+      "tag": {
+        "location": 3,
+        "text": "标签"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "午后南杂",
+        "desc": "Enjoy when you can, and endure when you must.",
+        "email": "1156743527@qq.com",
+        "link": "https://www.recoluan.com"
+      },
+      {
+        "title": "vuepress-theme-reco",
+        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        "link": "https://vuepress-theme-reco.recoluan.com"
+      }
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "XiaoQi",
+    "authorAvatar": "/avatar.png",
+    "record": "xxxx",
+    "startYear": "2017"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
---
+++ blogs/Vue/2020/test.md
@@ -0,0 +1,11 @@
+---
+title: second page in category1
+date: 2021-06-23
+tags:
+ - Vue
+categories:
+ - Vue
+---
+
+second page in category1
+# hello
